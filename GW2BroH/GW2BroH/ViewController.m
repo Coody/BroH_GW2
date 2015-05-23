6d9e3b7fc60c16449edbaa8c78ceb4fe0c4e192c
Coody <coody0829@gmail.com>
2015-05-24 01:42:18 +0800

    建立專案
    1. 專案建立
    
    2. 安裝 podfile （使用 AFNetworking 來實作 WebApi ）

---
+++ GW2BroH/GW2BroH/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GW2BroH
+//
+//  Created by CoodyChou on 2015/5/23.
+//  Copyright (c) 2015年 CoodyChou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BroH_GW2
 為了 GW2 所做的 iOS App
+
+# GW2 WebApi 參考網站
+
+https://wiki.guildwars2.com/wiki/API:Main
+
+# 第三方 framework
+
+AFNetworking
+
+# 專案使用方式
+
+1.下載後，請打開有 podfile 的資料夾，並且在此資料夾路徑下，輸入 pod install 來安裝第三方 framework.
