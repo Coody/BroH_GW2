a553e6bbf6ae7cfdc42fc91bcee82537f674d7d7
Coody <coody0829@gmail.com>
2015-07-27 13:35:42 +0800

    1. 加入註解

--- GW2BroH/GW2BroH/Tools/GW2BroH_Tools.h
+++ GW2BroH/GW2BroH/Tools/GW2BroH_Tools.h
@@ -12,14 +12,23 @@
 
 @interface GW2BroH_Tools : NSObject
 
+/**
+ * @brief - 取得 image 的本地端路徑
+ * @param - tempClass: 取得 image view 得 viewcontroller 名稱（通常只要輸入 [ViewControllerOOXX class] 就好）
+ * @param - tempImageName: image 的名稱（不帶 .png ）
+ */
 +(NSString *)getPathWithClass:(id)tempClass withImageName:(NSString *)tempImageName;
 
+/***/
 +(UIImage *)getImageWithClass:(id)tempClass withImageName:(NSString *)tempImageName;
 
+/***/
 +(NSString *)getPathWithString:(NSString*)tempString withImageName:(NSString *)tempImageName;
 
+/***/
 +(UIImage *)getImageWithString:(NSString*)tempString withImageName:(NSString *)tempImageName;
 
+/***/
 +(float) statusBarHeight;
 
 @end
